14dbbdbe3575cf1764fb92062f615078cbaa4a49
jiangyunkai <jiangyunkai@kuaishou.com>
2023-12-12 20:09:06 +0800

    init

---
+++ src/test/java/com/jiang/practice/MiniSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiang.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
